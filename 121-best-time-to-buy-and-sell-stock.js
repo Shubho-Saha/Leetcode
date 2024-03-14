0889ab37692516a05d3347c78cd831edfdb5a515
shubho-saha <shuvo7242@gmail.com>
2024-03-15 01:31:01 +0600

    lc-121 optimized 15/3/24

--- 121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock.js
@@ -16,3 +16,26 @@ var maxProfit = function(prices) {
     }
     return profit;
 };
+
+
+// More optimized solution.
+function sellStock(price) {
+    if (price.length < 2) return 0;
+    let left = 0; // buy
+    let right = 1; // sell
+    let maxProfit = 0;
+
+    while(right < price.length) {
+        if (price[right] > price[left]) {
+            let profit = price[right] - price[left];
+            maxProfit = Math.max(profit, maxProfit);
+        } else {
+            left = right;
+        }
+        right++;
+    }
+
+    return maxProfit;
+}
+
+console.log(sellStock([7,1,5,3,6,4]));
