b09c57fb64f59e8b2aaa110533c99aa51bb3bce8
shubho-saha <shuvo7242@gmail.com>
2024-03-15 02:31:26 +0600

    lc-1822 optimized 15/3/24

--- 1822-sign-of-the-product-of-an-array.js
+++ 1822-sign-of-the-product-of-an-array.js
@@ -14,3 +14,16 @@ var arraySign = function(nums) {
     if (zero) return 0;
     return (minusCount % 2) === 0 ? 1: -1;
 };
+
+//More optimized solution
+
+function arrSign(numbers) {
+    let sign = 1;
+    for(let n of numbers) {
+        if (n === 0) return 0;
+        if (n < 0) {
+            sign = - sign;
+        }
+    }
+    return sign;
+}
